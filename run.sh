8b3155c1eaa91c36002de3505609b583090fed6b
lay9366 <167525387+lay9366@users.noreply.github.com>
2024-04-22 20:30:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ echo "GitHub开源地址：https://github.com/sxbai/alist-on-replit"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O .replit https://github.com/sxbai/alist-on-replit/raw/master/.replit
-wget -O replit.nix https://github.com/sxbai/alist-on-replit/raw/master/replit.nix
+wget -O .replit https://github.com/lay9366/2130561/raw/master/.replit
+wget -O replit.nix https://github.com/lay9366/2130561/raw/master/replit.nix
 cd ..
-wget -O main.sh https://github.com/sxbai/alist-on-replit/raw/master/main.sh
+wget -O main.sh https://github.com/lay9366/2130561/raw/master/main.sh
 wget -O sx https://down.sxbai.repl.co/alist/v3.15.0/alist3.15.0
 cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
